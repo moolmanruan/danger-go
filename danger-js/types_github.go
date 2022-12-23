5b2af2756955396b92eb6ac81315b973080dfd61
Ruan Moolman <moolman.ruan@gmail.com>
2022-12-23 13:01:12 +0200

    Support github actions

--- danger-js/types_github.go
+++ danger-js/types_github.go
@@ -3,30 +3,16 @@ package danger_js
 import "time"
 
 type GitHub struct {
-	Issue              GitHubIssue              `json:"issue"`
-	PR                 GitHubPR                 `json:"pr"`
-	Commits            []GitHubCommit           `json:"commits"`
-	Reviews            []GitHubReview           `json:"reviews"`
-	RequestedReviewers GitHubRequestedReviewers `json:"requested_reviewers"`
-	ThisPR             GitHubThisPR             `json:"thisPR"` // Note: Not include in danger-kotlin
+	Issue              GitHubIssue     `json:"issue"`
+	PR                 GitHubPR        `json:"pr"`
+	ThisPR             GitHubAPIPR     `json:"thisPR"`
+	Commits            []GitHubCommit  `json:"commits"`
+	Reviews            []GitHubReview  `json:"reviews"`
+	RequestedReviewers GitHubReviewers `json:"requested_reviewers"`
 }
 
 type GitHubIssue struct {
-	ID           int64              `json:"id"`
-	Number       int64              `json:"number"`
-	Title        string             `json:"title"`
-	User         GitHubUser         `json:"user"`
-	State        string             `json:"state"` // "closed" | "open" | "locked"
-	Locked       bool               `json:"locked"`
-	Body         string             `json:"body,omitempty"`
-	CommentCount int                `json:"comments"`
-	Assignee     GitHubUser         `json:"assignee,omitempty"`
-	Assignees    []GitHubUser       `json:"assignees"`
-	Milestone    GitHubMilestone    `json:"milestone"`
-	CreatedAt    time.Time          `json:"created_at"`
-	UpdatedAt    time.Time          `json:"updated_at"`
-	ClosedAt     time.Time          `json:"closed_at"`
-	Labels       []GitHubIssueLabel `json:"labels"`
+	Labels []GitHubIssueLabel `json:"labels"`
 }
 
 type GitHubIssueLabel struct {
@@ -41,33 +27,34 @@ type GitHubUser struct {
 	Login     string `json:"login"`
 	Type      string `json:"type"` // "User" | "Organization" | "Bot"
 	AvatarURL string `json:"avatar_url"`
+	HRef      string `json:"href"`
 }
 
 type GitHubPR struct {
-	ID                 int64           `json:"id"`
-	Number             int             `json:"number"`
-	Title              string          `json:"title"`
-	Body               string          `json:"body"`
-	User               GitHubUser      `json:"user"`
-	Assignee           GitHubUser      `json:"assignee"`
-	Assignees          []GitHubUser    `json:"assignees"`
-	CreatedAt          time.Time       `json:"created_at"`
-	UpdatedAt          time.Time       `json:"updated_at"`
-	ClosedAt           time.Time       `json:"closed_at,omitempty"`
-	MergedAt           time.Time       `json:"merged_at,omitempty"`
-	Head               GitHubMergeRef  `json:"head"`
-	Base               GitHubMergeRef  `json:"base"`
-	State              string          `json:"state"` // "closed" | "open" | "merged" | "locked"
-	Locked             bool            `json:"locked"`
-	Merged             bool            `json:"merged"`
-	CommitCount        int             `json:"commits"`
-	CommentCount       int             `json:"comments"`
-	ReviewCommentCount int             `json:"review_comments"`
-	Additions          int             `json:"additions"`
-	Deletions          int             `json:"deletions"`
-	ChangedFiles       int             `json:"changed_files"`
-	Milestone          GitHubMilestone `json:"milestone"`
-	HTMLURL            string          `json:"html_url"`
+	Number            int            `json:"number"`
+	State             string         `json:"state"` // "closed" | "open" | "merged" | "locked"
+	Locked            bool           `json:"locked"`
+	Title             string         `json:"title"`
+	Body              string         `json:"body"`
+	CreatedAt         time.Time      `json:"created_at"`
+	UpdatedAt         time.Time      `json:"updated_at"`
+	ClosedAt          time.Time      `json:"closed_at,omitempty"`
+	MergedAt          time.Time      `json:"merged_at,omitempty"`
+	Head              GitHubMergeRef `json:"head"`
+	Base              GitHubMergeRef `json:"base"`
+	User              GitHubUser     `json:"user"`
+	Assignee          GitHubUser     `json:"assignee"`
+	Assignees         []GitHubUser   `json:"assignees"`
+	Draft             bool           `json:"draft"`
+	Merged            bool           `json:"merged"`
+	Comments          int            `json:"comments"`
+	ReviewComments    int            `json:"review_comments"`
+	Commits           int            `json:"commits"`
+	Additions         int            `json:"additions"`
+	Deletions         int            `json:"deletions"`
+	ChangedFiles      int            `json:"changed_files"`
+	HTMLURL           string         `json:"html_url"`
+	AuthorAssociation string         `json:"author_association"` // "COLLABORATOR", "CONTRIBUTOR", "FIRST_TIMER", "FIRST_TIME_CONTRIBUTOR", "MEMBER", "NONE", "OWNER"
 }
 type GitHubMergeRef struct {
 	Label string     `json:"label"`
@@ -88,24 +75,25 @@ type GitHubRepo struct {
 }
 
 type GitHubCommit struct {
+	Commit    GitCommit  `json:"commit"`
 	SHA       string     `json:"sha"`
 	URL       string     `json:"url"`
 	Author    GitHubUser `json:"author"`
-	Commit    Commit     `json:"commit"` // Should we create a custom Commit struct?
 	Committer GitHubUser `json:"committer"`
+	Parents   []any      `json:"parents"`
 }
 
 type GitHubReview struct {
 	User     GitHubUser `json:"user"`
-	ID       int64      `json:"id"`
-	Body     string     `json:"body"`
-	CommitID string     `json:"commit_id"`
-	State    string     `json:"state"` // APPROVED | CHANGES_REQUESTED | COMMENTED | PENDING | DISMISSED
+	ID       int64      `json:"id,omitempty"`
+	Body     string     `json:"body,omitempty"`
+	CommitID string     `json:"commit_id,omitempty"`
+	State    string     `json:"state,omitempty"` // APPROVED | CHANGES_REQUESTED | COMMENTED | PENDING | DISMISSED
 }
 
-type GitHubRequestedReviewers struct {
-	Users []string `json:"users"`
-	Teams []string `json:"teams"`
+type GitHubReviewers struct {
+	Users []GitHubUser `json:"users"`
+	Teams []any        `json:"teams"`
 }
 
 type GitHubMilestone struct {
@@ -123,8 +111,8 @@ type GitHubMilestone struct {
 	DueOn        time.Time  `json:"due_on"`
 }
 
-type GitHubThisPR struct {
-	Number int    `json:"number"`
-	Repo   string `json:"repo"`
+type GitHubAPIPR struct {
 	Owner  string `json:"owner"`
+	Repo   string `json:"repo"`
+	Number int    `json:"number"`
 }
