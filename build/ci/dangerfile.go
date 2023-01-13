ccc05f3ee55ead412520070cf8bba7eb3a8349be
Ruan Moolman <moolman.ruan@gmail.com>
2023-01-13 23:59:34 +0200

    Update dangerfile.go to use danger-go v0.0.3 (#5)

--- build/ci/dangerfile.go
+++ build/ci/dangerfile.go
@@ -3,23 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/moolmanruan/danger-go/cmd/danger-go/runner"
-	danger_js "github.com/moolmanruan/danger-go/danger-js"
+	"github.com/moolmanruan/danger-go"
+	"github.com/moolmanruan/danger-go/danger-js"
 )
 
 // Run is invoked by danger-go
-func Run(pr danger_js.PR) runner.DangerResults {
-	resp := runner.DangerResults{
-		Fails:     []runner.Violation{},
-		Messages:  []runner.Violation{},
-		Warnings:  []runner.Violation{},
-		Markdowns: []runner.Violation{},
-	}
-	addMessage := func(text string) {
-		resp.Messages = append(resp.Messages, runner.Violation{Message: text})
-	}
-
-	addMessage(fmt.Sprintf("%d new files added!", len(pr.Danger.Git.CreateFiles)))
-	addMessage(fmt.Sprintf("%d files modified!", len(pr.Danger.Git.ModifiedFiles)))
-	return resp
+func Run(d *danger.T, pr dangerJs.DSL) {
+	d.Message(fmt.Sprintf("%d new files added!", len(pr.Git.CreateFiles)), "", 0)
+	d.Message(fmt.Sprintf("%d files modified!", len(pr.Git.ModifiedFiles)), "", 0)
 }
